49534e656d6b7241315428270a558304d532eb85
Bart Burkhardt <bart.burkhardt@gmail.com>
2021-04-06 20:02:06 +0200

    added ignore regex pattern to not purge the 3DUtrecht files

--- sync.py
+++ sync.py
@@ -2,5 +2,6 @@ from dirsync import sync
 source_path = '/3DAmsterdam.fork/3DAmsterdam'
 target_path = '3DNetherlands'
 
-sync(source_path, target_path, 'sync', purge = True, create = True, verbose = True) #for syncing one way
-#sync(target_path, source_path, 'sync') #for syncing the opposite way
+pattern = ('.*3DUtrecht.*$','.*Fullscreen3DUtrecht.*$')
+
+sync(source_path, target_path, 'sync', purge = True, create = True, verbose = True, ignore= pattern) 
