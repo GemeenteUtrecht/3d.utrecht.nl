2b4c9111c52dad0c9c4aa94c215a48cc274e2a10
Bart Burkhardt <bart.burkhardt@gmail.com>
2021-04-07 12:10:49 +0200

    improved ignore regex pattern

--- sync.py
+++ sync.py
@@ -1,7 +1,5 @@
 from dirsync import sync
 source_path = '/3DAmsterdam.fork/3DAmsterdam'
 target_path = '3DNetherlands'
-
-pattern = ('.*3DUtrecht.*$','.*Fullscreen3DUtrecht.*$')
-
+pattern = ('.*3DUtrecht.*$','.*Fullscreen3DUtrecht.*$','^Library.*$','^Temp.*$', '.*Plugins.*.meta$')
 sync(source_path, target_path, 'sync', purge = True, create = True, verbose = True, ignore= pattern) 
