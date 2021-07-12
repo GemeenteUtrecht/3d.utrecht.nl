0cc1f8b0905c57765cdf83f5cb07596bb517c60e
Bart Burkhardt <bart.burkhardt@gmail.com>
2021-07-12 21:54:14 +0200

    - added polygon Triangulator - started with polygon layer

--- 3DNetherlands/Assets/CreatePolygon.cs
+++ 3DNetherlands/Assets/CreatePolygon.cs
@@ -4,27 +4,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using UnityEditor;
 using UnityEngine;
 
 public class CreatePolygon : MonoBehaviour
 {
-    public Material LineMaterial;
-    
+    public Material BebouwingMaterial;
+    public Material PerceelMaterial;
+
+    public Transform Bebouwing;
+    public Transform Perceel;
+
     void Start()
     {
-
-        //List<Vector2> polygon = new List<Vector2>();
-        //polygon.Add(new Vector2(0, 0));
-        //polygon.Add(new Vector2(0, 4));
-        //polygon.Add(new Vector2(2, 3));
-        //polygon.Add(new Vector2(4, 4));
-        //polygon.Add(new Vector2(4, 0));
-        //polygon.Add(new Vector2(2, 1));
-        ////polygon.Add(new Vector2(0, 0));
-
-        //RenderPolygon(polygon.ToArray(), LineMaterial, gameObject);
-
-
         //# Amsterdam
         //# bbox_min_x = 109000
         //# bbox_min_y = 474000
@@ -38,12 +30,59 @@ public class CreatePolygon : MonoBehaviour
         //bbox_max_y = 464000
 
         //string bbox = "150000,460000,15800,467000";
-        string bbox = "109000,474000,141000,501000";
+        //string bbox = "109000,474000,141000,501000";
+        string bbox = "123000,486000,124000,487000";
         string url = $"https://geodata.nationaalgeoregister.nl/kadastralekaart/wfs/v4_0?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=kadastralekaartv4:perceel&STARTINDEX=0&COUNT=1000&SRSNAME=urn:ogc:def:crs:EPSG::28992&BBOX={bbox},urn:ogc:def:crs:EPSG::28992&outputFormat=json";
         string url_bebouwing = $"https://geodata.nationaalgeoregister.nl/kadastralekaart/wfs/v4_0?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=kadastralekaartv4:bebouwing&STARTINDEX=0&COUNT=1000&SRSNAME=urn:ogc:def:crs:EPSG::28992&BBOX={bbox},urn:ogc:def:crs:EPSG::28992&outputFormat=json";
-        //var polygons = GetPerceelPolygon(url);
 
-        var polygons = GetBebouwingPolygons(url_bebouwing);
+        var testpolygon = GetTestPolygon();
+        var testpolygon2 = GetTestPolygon2();
+        var testpolygonSquareClockwise = GetTestPolygonSquareClockwise();
+        var testpolygonSquareCounterClockwise = GetTestPolygonSquareCounterClockwise();
+        var polygonsPerceel = GetPerceelPolygon(url);
+        var polygonsBebouwing = GetBebouwingPolygons(url_bebouwing);
+
+        //var center = GetCenter(testpolygonSquareClockwise);
+        //var center = GetCenter(testpolygonSquareCounterClockwise);
+        //var center = GetCenter(testpolygonSquare);
+        //var center = GetCenter(testpolygon2);
+        //var center = GetCenter(testpolygon);
+        var center = GetCenter(polygonsPerceel);
+        //var center = GetCenter(polygonsBebouwing);
+
+        //RenderPolygons(testpolygon2, center, PerceelMaterial, Perceel);
+        //RenderPolygons(testpolygon, center, PerceelMaterial, Perceel);
+        RenderPolygons(polygonsPerceel, center, PerceelMaterial, Perceel);
+
+        //RenderConvexPolygons(polygonsBebouwing.Take(1).ToList(), center, BebouwingMaterial, Bebouwing);
+        RenderConvexPolygons(polygonsBebouwing, center, BebouwingMaterial, Bebouwing);
+        //RenderConvexPolygons(testpolygon, center, BebouwingMaterial, Bebouwing);
+        //RenderConvexPolygons(testpolygon2, center, BebouwingMaterial, Bebouwing);
+        //RenderConvexPolygons(testpolygonSquareClockwise, center, BebouwingMaterial, Bebouwing);
+        //RenderConvexPolygons(testpolygonSquareCounterClockwise, center, BebouwingMaterial, Bebouwing);
+
+
+    }
+
+
+    void RenderConvexPolygons(List<Vector2[]> polygons, Vector2 center, Material material, Transform parent)
+    {
+        foreach (var polygon in polygons)
+        {            
+            RenderPolygonConvex(polygon, material, center, parent);
+        }
+    }
+
+
+    void RenderPolygons(List<Vector2[]> polygons, Vector2 center, Material material, Transform parent){
+        foreach (var polygon in polygons)
+        {
+            RenderPolygon(polygon, material, center, parent);
+}
+    }
+
+    Vector2 GetCenter(List<Vector2[]> polygons)
+    {
         var minx = polygons.Min(l => l.Min(o => o.x));
         var maxx = polygons.Max(l => l.Max(o => o.x));
         var miny = polygons.Min(l => l.Min(o => o.y));
@@ -52,14 +91,8 @@ public class CreatePolygon : MonoBehaviour
         var centerx = minx + ((maxx - minx) / 2);
         var centery = miny + ((maxy - miny) / 2);
 
-        Vector2 center = new Vector2( centerx, centery );
-
-
-        foreach (var polygon in polygons)
-        {
-            RenderPolygon(polygon, LineMaterial, gameObject, center);
-        }
-
+        Vector2 center = new Vector2(centerx, centery);
+        return center;
     }
 
     List<Vector2[]> GetPerceelPolygon(string url)
@@ -79,28 +112,17 @@ public class CreatePolygon : MonoBehaviour
             {
                 List<Vector2> polygonList = new List<Vector2>();
 
-                var offsetX = feature.properties.perceelnummerPlaatscoordinaatX;
-                var offsetY = feature.properties.perceelnummerPlaatscoordinaatY;
-
-                var deltaX = feature.properties.perceelnummerVerschuivingDeltaX;
-                var deltaY = feature.properties.perceelnummerVerschuivingDeltaY;
-
-                //Debug.Log($"perceelnummerVerschuivingDeltaX: {feature.properties.perceelnummerVerschuivingDeltaX} ");
-                //Debug.Log($"perceelnummerRotatie: {feature.properties.perceelnummerRotatie} ");
-
                 var coordinates = feature.geometry.coordinates;
                 foreach(var points in coordinates)
                 {
                     foreach(var point in points)
                     {
-                        polygonList.Add( new Vector2( point[0]-offsetX + deltaX, 
-                                                      point[1]-offsetY + deltaY));
+                        polygonList.Add(new Vector2(point[0], point[1]));
                     }
                 }
                 list.Add(polygonList.ToArray());
             }           
         }
-
 
         return list;
     }
@@ -138,8 +160,93 @@ public class CreatePolygon : MonoBehaviour
         return list;
     }
 
-    void RenderPolygon(Vector2[] polygonPoints, Material lineMaterial, GameObject gam, Vector2 center)
+    List<Vector2[]> GetTestPolygon()
     {
+        List<Vector2[]> polygons = new List<Vector2[]>();
+        List<Vector2> polygon = new List<Vector2>();
+        polygon.Add(new Vector2(0, 0));
+        polygon.Add(new Vector2(0, 4));
+        polygon.Add(new Vector2(2, 3));
+        polygon.Add(new Vector2(10, 4));
+        polygon.Add(new Vector2(4, 0));
+        polygon.Add(new Vector2(2, 1));
+        polygons.Add(polygon.ToArray());
+        //polygon.Add(new Vector2(0, 0));
+        return polygons;                
+    }
+
+    List<Vector2[]> GetTestPolygonSquareClockwise()
+    {
+        List<Vector2[]> polygons = new List<Vector2[]>();
+        List<Vector2> polygon = new List<Vector2>();
+        polygon.Add(new Vector2(0, 0));
+        polygon.Add(new Vector2(0, 4));
+        polygon.Add(new Vector2(4, 4));
+        polygon.Add(new Vector2(4, 0));      
+        polygons.Add(polygon.ToArray());        
+        return polygons;
+    }
+
+    List<Vector2[]> GetTestPolygonSquareCounterClockwise()
+    {
+        List<Vector2[]> polygons = new List<Vector2[]>();
+        List<Vector2> polygon = new List<Vector2>();
+        polygon.Add(new Vector2(1, 0));
+        polygon.Add(new Vector2(4, 0));
+        polygon.Add(new Vector2(4, 4));
+        polygon.Add(new Vector2(0, 4));
+        polygons.Add(polygon.ToArray());
+        return polygons;
+    }
+
+    List<Vector2[]> GetTestPolygon2()
+    {
+        List<Vector2[]> polygons = new List<Vector2[]>();
+        List<Vector2> polygon = new List<Vector2>();
+
+        polygon.Add(new Vector2(364.6016f, -521.6875f));
+        polygon.Add(new Vector2(364.5938f,-521.7188f));
+        polygon.Add(new Vector2(371.2031f,-523.875f));
+        polygon.Add(new Vector2(371.8281f,-524.0625f));
+        polygon.Add(new Vector2(374.4531f,-515.9375f));
+        polygon.Add(new Vector2(373.4844f,-515.625f));
+        polygon.Add(new Vector2(374.2188f,-513.4063f));
+        polygon.Add(new Vector2(375.1797f,-513.75f));
+        polygon.Add(new Vector2(378.3828f,-503.9688f));
+        polygon.Add(new Vector2(376.4844f,-503.375f));
+        polygon.Add(new Vector2(376.8516f,-502.25f));
+        polygon.Add(new Vector2(373.7188f,-499.375f));
+        polygon.Add(new Vector2(372.6094f,-499.8125f));
+        polygon.Add(new Vector2(371.9141f,-498f));
+        polygon.Add(new Vector2(307.1641f,-524.125f));
+        polygon.Add(new Vector2(311.625f,-535.3438f));
+        polygon.Add(new Vector2(357.2422f,-516.9375f));
+        polygon.Add(new Vector2(355.5938f,-512.75f));
+        polygon.Add(new Vector2(360.5938f,-510.7813f));
+        polygon.Add(new Vector2(360.2656f,-509.9688f));
+        polygon.Add(new Vector2(362.8906f,-508.9063f));
+        polygon.Add(new Vector2(364.1563f,-508.3438f));
+        polygon.Add(new Vector2(364.4766f,-508.75f));
+        polygon.Add(new Vector2(364.1484f,-509.0313f));
+        polygon.Add(new Vector2(364.7578f,-510.375f));
+        polygon.Add(new Vector2(366.6016f,-512.125f));
+        polygon.Add(new Vector2(367.9766f,-512.6563f));
+        polygon.Add(new Vector2(368.2578f,-512.3438f));
+        polygon.Add(new Vector2(368.6484f,-512.6563f));
+        polygon.Add(new Vector2(368.2109f,-513.9688f));
+        polygon.Add(new Vector2(367.2266f,-516.5938f));
+        polygon.Add(new Vector2(366.375f,-516.3125f));
+       // polygon.Add(new Vector2(364.6016f,-521.6875f));
+
+        polygons.Add(polygon.ToArray());
+        return polygons;
+    }
+
+
+
+    void RenderPolygon(Vector2[] polygonPoints, Material lineMaterial, Vector2 center, Transform parent)
+    {
+
         List<int> indices = new List<int>();
         List<Vector3> points = new List<Vector3>();
 
@@ -161,6 +268,7 @@ public class CreatePolygon : MonoBehaviour
         }
 
         GameObject newgameobject = new GameObject();
+        newgameobject.transform.parent = parent;
 
         MeshFilter filter = newgameobject.AddComponent<MeshFilter>();
        // newgameobject.AddComponent<Renderer>();
@@ -172,5 +280,52 @@ public class CreatePolygon : MonoBehaviour
         filter.sharedMesh = mesh;
     }
 
+    //http://wiki.unity3d.com/index.php?title=Triangulator
+    void RenderPolygonConvex(Vector2[] polygonPoints, Material lineMaterial, Vector2 center, Transform parent)
+    {
 
+        if (polygonPoints.First().Equals( polygonPoints.Last()))
+        {
+            polygonPoints = polygonPoints.Take(polygonPoints.Length - 1).ToArray();            
+        }
+
+
+        // Use the triangulator to get indices for creating triangles
+        Triangulator tr = new Triangulator(polygonPoints);
+        int[] indices = tr.Triangulate();
+
+        Vector3[] vertices = new Vector3[polygonPoints.Length];
+        for (int i = 0; i < polygonPoints.Length; i++)
+        {
+           vertices[i] = new Vector3(polygonPoints[i].x - center.x, 0, polygonPoints[i].y - center.y);
+
+           //debug object to show the points
+           var debugobject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+           debugobject.transform.position = vertices[i];
+           debugobject.transform.localScale = Vector3.one * 0.4f;
+           debugobject.name = $"{i + 1}";
+           Destroy(debugobject.GetComponent<Collider>());
+
+           Debug.Log($"x:{vertices[i].x} y:{vertices[i].z}");
+
+        }
+
+        Mesh msh = new Mesh();
+        msh.vertices = vertices;
+        msh.triangles = indices;
+
+        //var normals = Enumerable.Repeat( new Vector3(0,1,0) , polygonPoints.Length).ToArray();
+        //msh.normals = normals;
+
+        msh.RecalculateNormals();
+        msh.RecalculateBounds();
+
+        GameObject gam = new GameObject();
+        gam.transform.parent = parent;
+        gam.AddComponent<MeshRenderer>().material = lineMaterial;
+        gam.AddComponent<MeshFilter>().mesh = msh;
+    }
 }
+    
+
+
---
+++ 3DNetherlands/Assets/PolygonLayer.cs
@@ -0,0 +1,182 @@
+using Netherlands3D.LayerSystem;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+
+public class PolygonLayer : Layer
+{
+    public override void HandleTile(TileChange tileChange, Action<TileChange> callback = null)
+    {
+		TileAction action = tileChange.action;
+		var tileKey = new Vector2Int(tileChange.X, tileChange.Y);
+		switch (action)
+		{
+			case TileAction.Create:
+				Tile newTile = CreateNewTile(tileChange, callback);
+				tiles.Add(tileKey, newTile);
+				break;
+			case TileAction.Remove:
+				InteruptRunningProcesses(tileKey);
+				RemoveTile(tileChange, callback);
+				return;
+			default:
+				callback(tileChange);
+				break;
+		}
+	}
+
+	private Tile CreateNewTile(TileChange tileChange, System.Action<TileChange> callback = null)
+	{
+		Tile tile = new Tile();
+		tile.LOD = 0;
+		tile.tileKey = new Vector2Int(tileChange.X, tileChange.Y);
+		tile.layer = transform.gameObject.GetComponent<Layer>();
+		tile.gameObject = new GameObject("perceel-" + tileChange.X + "_" + tileChange.Y);
+		tile.gameObject.transform.parent = transform.gameObject.transform;
+		tile.gameObject.layer = tile.gameObject.transform.parent.gameObject.layer;
+		tile.gameObject.SetActive(false);
+		Generate(tileChange, tile, callback);
+		return tile;
+	}
+
+	public void Generate(TileChange tileChange, Tile tile, System.Action<TileChange> callback = null)
+	{
+		tile.runningCoroutine = StartCoroutine(BuildLineNetwork(tileChange, tile, callback));
+	}
+
+	IEnumerator BuildLineNetwork(TileChange tileChange, Tile tile, Action<TileChange> callback = null)
+	{
+		var bbox = tile.tileKey.x + "," + tile.tileKey.y + "," + (tile.tileKey.x + tileSize) + "," + (tile.tileKey.y + tileSize);
+		string url = $"https://geodata.nationaalgeoregister.nl/kadastralekaart/wfs/v4_0?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=kadastralekaartv4:perceel&STARTINDEX=0&COUNT=1000&SRSNAME=urn:ogc:def:crs:EPSG::28992&BBOX={bbox},urn:ogc:def:crs:EPSG::28992&outputFormat=json";
+		var polygonsPerceel = GetPerceelPolygon(url);
+
+		RenderPolygons(polygonsPerceel, center, PerceelMaterial, Perceel);
+
+		//Finaly activate our new tile gameobject (if layer is not disabled)
+		tile.gameObject.SetActive(isEnabled);
+
+		yield return new WaitForEndOfFrame();
+
+		yield return null;
+		callback(tileChange);
+	}
+
+	List<Vector2[]> GetPerceelPolygon(string url)
+	{
+		List<Vector2[]> list = new List<Vector2[]>();
+
+		using (WebClient client = new WebClient())
+		using (Stream stream = client.OpenRead(url))
+		using (StreamReader streamReader = new StreamReader(stream))
+		using (JsonTextReader reader = new JsonTextReader(streamReader))
+		{
+			reader.SupportMultipleContent = true;
+			var serializer = new JsonSerializer();
+			JsonModels.WebFeatureService.WFSRootobject wfs = serializer.Deserialize<JsonModels.WebFeatureService.WFSRootobject>(reader);
+
+			foreach (var feature in wfs.features)
+			{
+				List<Vector2> polygonList = new List<Vector2>();
+
+				var coordinates = feature.geometry.coordinates;
+				foreach (var points in coordinates)
+				{
+					foreach (var point in points)
+					{
+						polygonList.Add(new Vector2(point[0], point[1]));
+					}
+				}
+				list.Add(polygonList.ToArray());
+			}
+		}
+
+		return list;
+	}
+
+	void RenderPolygons(List<Vector2[]> polygons, Vector2 center, Material material, Transform parent)
+	{
+		
+		foreach (var polygon in polygons)
+		{
+			List<int> indices = new List<int>();
+			List<Vector3> points = new List<Vector3>();
+
+			foreach (var point in polygon)
+			{
+				points.Add(new Vector3(point.x - center.x, 0, point.y - center.y));
+			}
+
+			for (int i = 0; i < polygon.Length-1; i++)
+			{
+				indices.Add(i);
+				indices.Add(i+1);
+			}
+		}
+	}
+
+	void RenderPolygon(Vector2[] polygonPoints, Material lineMaterial, Vector2 center, Transform parent)
+	{
+
+		List<int> indices = new List<int>();
+		List<Vector3> points = new List<Vector3>();
+
+		foreach (var point in polygonPoints)
+		{
+			points.Add(new Vector3(point.x - center.x, 0, point.y - center.y));
+		}
+
+		for (int i = 0; i < polygonPoints.Length; i++)
+		{
+			indices.Add(i);
+
+			if (i == polygonPoints.Length - 1)
+			{
+				indices.Add(0);
+			}
+			else
+			{
+				indices.Add(i + 1);
+			}
+		}
+
+		GameObject newgameobject = new GameObject();
+		newgameobject.transform.parent = parent;
+
+		MeshFilter filter = newgameobject.AddComponent<MeshFilter>();
+		// newgameobject.AddComponent<Renderer>();
+		newgameobject.AddComponent<MeshRenderer>().material = lineMaterial;
+
+		var mesh = new Mesh();
+		mesh.vertices = points.ToArray();
+		mesh.SetIndices(indices.ToArray(), MeshTopology.Lines, 0);
+		filter.sharedMesh = mesh;
+	}
+
+	private void RemoveTile(TileChange tileChange, System.Action<TileChange> callback = null)
+	{
+		var tileKey = new Vector2Int(tileChange.X, tileChange.Y);
+		if (tiles.ContainsKey(tileKey))
+		{
+			Tile tile = tiles[tileKey];
+			if (tile.gameObject)
+			{
+				MeshFilter[] meshFilters = tile.gameObject.GetComponentsInChildren<MeshFilter>();
+
+				foreach (var meshfilter in meshFilters)
+				{
+					Destroy(meshfilter.sharedMesh);
+				}
+
+				Destroy(tile.gameObject);
+			}
+
+			tiles.Remove(tileKey);
+		}
+		callback(tileChange);
+	}
+
+}
